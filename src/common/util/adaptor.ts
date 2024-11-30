a3e5e79d2a38c68ab9d7836959ea142cf484c8d5
KangMins <rkdalsdl98@naver.com>
2024-11-30 17:20:07 +0900

    feat: 유저 API 연동 완료

--- src/common/util/adaptor.ts
+++ src/common/util/adaptor.ts
@@ -3,7 +3,11 @@
  * 
  * 확장을 고려해서 FlutterAppAdaptor 네임 스페이스로 묶어 관리하고 있습니다. </br>
  * 
- * 로그인 결과는 아래의 onSuccessLogin을 호출해 데이터를 앱에 전달할 수 있습니다.
+ * 로그인 결과는 아래의 onSuccessLogin을 호출해 데이터를 앱에 전달할 수 있습니다. </br>
+ * 
+ * 이벤트를 하나로 처리하려 했지만 회원가입과 로그인의 리스폰 객체 타입이 달라 두 개로 분리했습니다. </br>
+ * 
+ * sendLoginResult는 로그인의 결과를 sendSignUpWithLoginResult은 회원가입 이후 로그인의 결과를 전달합니다.
  */
 export namespace FlutterAppAdaptor {
     export const sendLoginResult = ({
